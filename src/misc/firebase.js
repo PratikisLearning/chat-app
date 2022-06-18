e5a9626c2a6ab19e6e680693876ac5f357b912a7
Pratik <manpratik01@gmail.com>
2022-06-19 01:48:53 +0530

    fb and google login database

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,8 +1,11 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
     apiKey: "AIzaSyDQ_YFEoI4hyQO3i2QSofnNcdtmoXJT4Yo",
     authDomain: "chat-app-35187.firebaseapp.com",
+    databaseURL: "https://chat-app-35187-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "chat-app-35187",
     storageBucket: "chat-app-35187.appspot.com",
     messagingSenderId: "634970524718",
@@ -10,3 +13,5 @@ const config = {
   }
 
   const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
