c9574725c16b47bef123665d29f8370f7aac9a27
Pratik <manpratik01@gmail.com>
2022-06-18 23:49:13 +0530

    firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+    apiKey: "AIzaSyDQ_YFEoI4hyQO3i2QSofnNcdtmoXJT4Yo",
+    authDomain: "chat-app-35187.firebaseapp.com",
+    projectId: "chat-app-35187",
+    storageBucket: "chat-app-35187.appspot.com",
+    messagingSenderId: "634970524718",
+    appId: "1:634970524718:web:43bf2acfb7143dda1063bf"
+  }
+
+  const app = firebase.initializeApp(config);
